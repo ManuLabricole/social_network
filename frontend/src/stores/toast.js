d6ee602b07c78b8220bb03b5113f26c5c0d3e4fe
Manu Labricole <manu_barriol@hotmail.fr>
2023-09-07 23:38:22 +0200

    create toast component

---
+++ frontend/src/stores/toast.js
@@ -0,0 +1,24 @@
+import { defineStore } from "pinia";
+
+export const useToastStore = defineStore("toast", () => {
+    id: 'toast';
+    state: () => ({
+        ms: 0,
+        message: '',
+        classes: '',
+        isVisible: false,
+    });
+    actions: {
+        showToast(ms, message, classes) {
+            this.ms = parseInt(ms);
+            this.message = message;
+            this.classes = classes;
+            this.isVisible = true;
+
+            setTimeout(() => {
+                this.classes += '-translate-y-28';
+
+        }, 10);
+
+    }
+});
