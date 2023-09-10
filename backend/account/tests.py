75fde3c3916abcaa0d87bb112a372053b0655a68
Manu Labricole <manu_barriol@hotmail.fr>
2023-09-10 16:42:15 +0200

    add test

--- backend/account/tests.py
+++ backend/account/tests.py
@@ -1,6 +1,7 @@
+from .forms import SignupForm
 from django.test import TestCase
-from django.contrib.auth import get_user_model
 from account.models import User
+from .forms import SignupForm
 
 UserModel = User
 
@@ -30,3 +31,61 @@ class UserModelTest(TestCase):
         )
         self.assertTrue(admin_user.is_superuser)
         self.assertTrue(admin_user.is_staff)
+
+
+class SignupFormTest(TestCase):
+    def test_valid_form(self):
+        # Create a dictionary with valid data for the form
+        form_data = {
+            'email': 'test@example.com',
+            'name': 'Test User',
+            'password1': 'securepassword123',
+            'password2': 'securepassword123',
+        }
+
+        form = SignupForm(data=form_data)
+
+        # Assert that the form is valid
+        self.assertTrue(form.is_valid())
+
+    def test_password_mismatch(self):
+        # Create a dictionary with mismatched passwords
+        form_data = {
+            'email': 'test@example.com',
+            'name': 'Test User',
+            'password1': 'securepassword123',
+            'password2': 'differentpassword456',
+        }
+
+        form = SignupForm(data=form_data)
+
+        # Assert that the form is not valid due to password mismatch
+        self.assertFalse(form.is_valid())
+
+    def test_missing_required_fields(self):
+        # Create a dictionary with missing required fields
+        form_data = {
+            'email': 'test@example.com',
+            'name': '',  # Missing name
+            'password1': 'securepassword123',
+            'password2': 'securepassword123',
+        }
+
+        form = SignupForm(data=form_data)
+
+        # Assert that the form is not valid due to missing required fields
+        self.assertFalse(form.is_valid())
+
+    def test_invalid_email(self):
+        # Create a dictionary with an invalid email format
+        form_data = {
+            'email': 'invalid-email',  # Invalid email format
+            'name': 'Test User',
+            'password1': 'securepassword123',
+            'password2': 'securepassword123',
+        }
+
+        form = SignupForm(data=form_data)
+
+        # Assert that the form is not valid due to an invalid email format
+        self.assertFalse(form.is_valid())
