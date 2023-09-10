9a362a3c2cc49d50e583a98a431f14f373e183a1
Manu Labricole <manu_barriol@hotmail.fr>
2023-09-10 17:19:15 +0200

    add comment to user store

--- frontend/src/stores/user.js
+++ frontend/src/stores/user.js
@@ -1,26 +1,29 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
 
+// Define a Pinia store for managing user authentication and data.
 export const useUserStore = defineStore('user', {
 
     state: () => ({
         user: {
-            isAuthenticated: false,
-            id: null,
-            name: null,
-            email: null,
-            access: null,
-            refresh: null,
+            isAuthenticated: false, // Flag indicating user authentication status.
+            id: null,               // User ID.
+            name: null,             // User's name.
+            email: null,            // User's email.
+            access: null,           // Access token.
+            refresh: null,          // Refresh token.
         }
     }),
 
     actions: {
+        // Action to initialize the store based on locally stored authentication tokens.
         initStore() {
             console.log('initStore', localStorage.getItem('user.access'))
 
             if (localStorage.getItem('user.access')) {
                 console.log('User has access!')
 
+                // Retrieve and set user data from local storage.
                 this.user.access = localStorage.getItem('user.access')
                 this.user.refresh = localStorage.getItem('user.refresh')
                 this.user.id = localStorage.getItem('user.id')
@@ -28,28 +31,34 @@ export const useUserStore = defineStore('user', {
                 this.user.email = localStorage.getItem('user.email')
                 this.user.isAuthenticated = true
 
+                // Refresh the access token.
                 this.refreshToken()
 
                 console.log('Initialized user:', this.user)
             }
         },
 
+        // Action to set user authentication tokens and update the store.
         setToken(data) {
             console.log('setToken', data)
 
+            // Set user tokens and update authentication status.
             this.user.access = data.access
             this.user.refresh = data.refresh
             this.user.isAuthenticated = true
 
+            // Store tokens in local storage.
             localStorage.setItem('user.access', data.access)
             localStorage.setItem('user.refresh', data.refresh)
 
             console.log('user.access: ', localStorage.getItem('user.access'))
         },
 
+        // Action to remove user authentication tokens and clear user data.
         removeToken() {
             console.log('removeToken')
 
+            // Clear tokens and reset authentication status.
             this.user.refresh = null
             this.user.access = null
             this.user.isAuthenticated = false
@@ -57,6 +66,7 @@ export const useUserStore = defineStore('user', {
             this.user.name = false
             this.user.email = false
 
+            // Clear token data from local storage.
             localStorage.setItem('user.access', '')
             localStorage.setItem('user.refresh', '')
             localStorage.setItem('user.id', '')
@@ -64,13 +74,16 @@ export const useUserStore = defineStore('user', {
             localStorage.setItem('user.email', '')
         },
 
+        // Action to set user information and store it in local storage.
         setUserInfo(user) {
             console.log('setUserInfo', user)
 
+            // Set user information.
             this.user.id = user.id
             this.user.name = user.name
             this.user.email = user.email
 
+            // Store user information in local storage.
             localStorage.setItem('user.id', this.user.id)
             localStorage.setItem('user.name', this.user.name)
             localStorage.setItem('user.email', this.user.email)
@@ -78,20 +91,25 @@ export const useUserStore = defineStore('user', {
             console.log('User', this.user)
         },
 
+        // Action to refresh the access token using the refresh token.
         refreshToken() {
             axios.post('/api/v1/refresh/', {
                 refresh: this.user.refresh
             })
                 .then((response) => {
+                    // Update the access token.
                     this.user.access = response.data.access
 
+                    // Store the updated access token in local storage.
                     localStorage.setItem('user.access', response.data.access)
 
+                    // Set the authorization header for Axios to use the new access token.
                     axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.access
                 })
                 .catch((error) => {
                     console.log(error)
 
+                    // If token refresh fails, remove user tokens and log the user out.
                     this.removeToken()
                 })
         },
