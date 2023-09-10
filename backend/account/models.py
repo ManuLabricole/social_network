76fc8db669ed5308b2e152ad30cd985e0bcf5fff
Manu Labricole <manu_barriol@hotmail.fr>
2023-09-10 15:53:04 +0200

    User class

--- backend/account/models.py
+++ backend/account/models.py
@@ -80,4 +80,20 @@ class CustomUserManager(UserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    pass
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255, blank=True, default='')
+    avatar = models.ImageField(upload_to='avatars', blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(blank=True, null=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
