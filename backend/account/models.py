54e3b5a7ffe6f3508a06e10a3a9fd70dfdb15ad3
Manu Labricole <manu_barriol@hotmail.fr>
2023-09-11 22:00:23 +0200

    remove useless import

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,6 +1,6 @@
 import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionManager, PermissionsMixin, UserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.utils import timezone
 
 
