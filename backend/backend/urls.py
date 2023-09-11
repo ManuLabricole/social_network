39afde457df565b552d0ee7ec3a9dd5aa4427da3
Manu Labricole <manu_barriol@hotmail.fr>
2023-09-11 21:20:57 +0200

    redirect the '/' to login

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('api/v1/', include('account.urls')),
     path('admin/', admin.site.urls),
+    # Redirect '/' to 'api/v1/Home'
+    path('', RedirectView.as_view(url='/api/v1/login/')),
 ]
