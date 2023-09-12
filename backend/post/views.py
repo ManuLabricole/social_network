a99bedfb9d66eb95bcb56fb83abab3bc38f83a1b
Manu Labricole <manu_barriol@hotmail.fr>
2023-09-12 23:11:09 +0200

    add a post view for posts

--- backend/post/views.py
+++ backend/post/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
+from django.http import JsonResponse
 
 from .models import Post
 from .serializers import PostSerializer
@@ -10,3 +12,14 @@ class PostListView(generics.ListAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
     print(queryset)
+
+
+@api_view(['POST'])
+def post_create(request):
+    # serializer = PostSerializer(data=request.data)
+    # if serializer.is_valid():
+    #     serializer.save()
+    # return JsonResponse(serializer.data, status=201)
+    data = request.data
+    print(data)
+    return JsonResponse(data, status=201)
