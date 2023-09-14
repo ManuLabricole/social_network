389af723f99626f22d3ef655675b80203fc364dc
Manu Labricole <manu_barriol@hotmail.fr>
2023-09-14 15:29:54 +0200

    hanfle filtering of project depending on the request in the same API view

--- backend/post/views.py
+++ backend/post/views.py
@@ -2,23 +2,20 @@ from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from django.http import JsonResponse
+from django.db.models import Q
 
 from .models import Post
 from .serializers import PostSerializer
 
-from .forms import PostForm
-
-# Here we render the list of posts
-
 
 class PostListView(generics.ListCreateAPIView):
-    print("PostListView")
-    queryset = Post.objects.all()
+
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
 
+    # Here we rewrite the create method to add the author to the post
     def create(self, request):
-        print("Inside create method")
+
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(author=self.request.user)
@@ -28,6 +25,30 @@ class PostListView(generics.ListCreateAPIView):
             print(serializer.errors)
             response = JsonResponse(serializer.errors, status=400)
             return response
+
+    # Here we rewrite the get method to filter if needed
+    def get_queryset(self):
+        # Start with all posts
+        queryset = Post.objects.all()
+
+        # Filter by search query if provided
+        search_query = self.request.query_params.get(  # type: ignore
+            'search', None)
+        print("Search query:", search_query)
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query) |
+                Q(content__icontains=search_query) |
+                # Assuming you have a username field in your user model
+                Q(author__username__icontains=search_query)
+            )
+
+        # Filter by user_id if provided
+        user_id = self.kwargs.get('user_id', None)
+        if user_id:
+            queryset = queryset.filter(author__id=user_id)
+
+        return queryset
 
 
 class UserPostsListView(generics.ListAPIView):
