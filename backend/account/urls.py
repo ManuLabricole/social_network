e5744636150feeb76642a2de761ed839607c5b4e
Manu Labricole <manu_barriol@hotmail.fr>
2023-09-14 12:03:20 +0200

    manage the details view for getting user infos from id

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import api
+from . import views
 
 urlpatterns = [
     path('me/', api.me, name='me'),
     path('signup/', api.signup, name='signup'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/<uuid:id>/',
+         views.UserDetailView.as_view(), name='user-detail'),
 ]
