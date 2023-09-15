56d195d37f9bd00a9d946226f5ec2a8a02e68be1
Manu Labricole <manu_barriol@hotmail.fr>
2023-09-15 13:32:06 +0200

    add a second UserProfile in friendshipp to extend the first User class

--- backend/friendship/models.py
+++ backend/friendship/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    friends = models.ManyToManyField('self', blank=True)
+    friendship_requests = models.ManyToManyField('self', blank=True)
+
+    def __str__(self) -> str:
+        return super().__str__()
