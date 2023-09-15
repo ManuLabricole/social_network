44dc47a2353b41c5a1a1b6301e1d083458ff4548
Manu Labricole <manu_barriol@hotmail.fr>
2023-09-15 15:49:47 +0200

    create Friendship component to fetch pending request

---
+++ frontend/src/components/FriendshipRequests.vue
@@ -0,0 +1,38 @@
+<template>
+	<div
+		class="p-4 space-y-4 bg-white border border-gray-200 text-center rounded-lg">
+		<p class="text-sm font-bold"><strong>Friend Requests</strong></p>
+	</div>
+</template>
+
+<script>
+	export default {
+		setup() {
+			return {};
+		},
+		data() {
+			return {
+				pendingRequests: [],
+			};
+		},
+		methods: {
+			async fetchPendingRequests() {
+				const response = await axios
+					.get('/api/friendship-requests')
+					.then((response) => {
+						console.log(response);
+						this.pendingRequests = response.data;
+						console.log(this.pendingRequests);
+					})
+					.catch((error) => {
+						console.log(error);
+					});
+			},
+		},
+		mounted() {
+			this.fetchPendingRequests();
+		},
+	};
+</script>
+
+<style></style>
