b3ab4faf85cc04f35ec2d884bea78eaba66a1cbf
Manu Labricole <manu_barriol@hotmail.fr>
2023-10-10 18:34:52 +0200

    migrate

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -8,10 +8,11 @@ class Conversation(models.Model):
     
 class ConversationMessage(models.Model):
     conversation = models.ForeignKey(Conversation, related_name="messages", on_delete=models.CASCADE)
-    sender = models.ForeignKey(User, related_name="messages", on_delete=models.SET_NULL, null=True)
-    text = models.TextField()
+    sender = models.ForeignKey(User, related_name="sent_messages", on_delete=models.SET_NULL, null=True)
+    receiver = models.ForeignKey(User, related_name="received_messages", on_delete=models.SET_NULL, null=True)
+    body= models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return self.text
+        return self.body
