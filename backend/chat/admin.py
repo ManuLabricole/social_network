76c7f42f9c1737cb40524f8273d1f87166ae49be
Manu Labricole <manu_barriol@hotmail.fr>
2023-10-15 17:05:26 +0200

    register models in admin pânel

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Conversation, ConversationMessage
 
-# Register your models here.
+
+# Register Conversation model
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ("id", "created_at", "modified_at")
+    search_fields = ("id",)
+    list_filter = ("created_at", "modified_at")
+
+
+# Register ConversationMessage model
+@admin.register(ConversationMessage)
+class ConversationMessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "conversation", "sender", "text", "created_at", "modified_at")
+    search_fields = ("text", "sender__username")
+    list_filter = ("created_at", "modified_at")
