70d4e221e4300f3b9d8a3c07c84644eb8cfb3997
Manu Labricole <manu_barriol@hotmail.fr>
2023-10-15 18:11:04 +0200

    -

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Conversation, ConversationMessage
+from .serializers import ConversationSerializer, ConversationMessageSerializer
 
-# Create your views here.
+
+# List all conversations for the logged-in user
+class ListConversationsView(generics.ListAPIView):
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Conversation.objects.filter(user=self.request.user)
+
+
+# Retrieve a single conversation along with its messages
+class RetrieveConversationView(generics.RetrieveAPIView):
+    serializer_class = ConversationSerializer  # You might need a different serializer to include messages
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Conversation.objects.filter(user=self.request.user)
+
+
+# Create a new message in a conversation
+class CreateMessageView(generics.CreateAPIView):
+    serializer_class = ConversationMessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # Logic to associate the message with a conversation and the sender
+        pass
+
+
+# Delete a message from a conversation
+class DeleteMessageView(generics.DestroyAPIView):
+    serializer_class = ConversationMessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return ConversationMessage.objects.filter(sender=self.request.user)
