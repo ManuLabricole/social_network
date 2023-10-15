48a203cb64d6bd801e778661eb09098d8b4ecd4f
Manu Labricole <manu_barriol@hotmail.fr>
2023-10-15 19:31:01 +0200

    change models and migrate

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from account.models import User
+from userprofile.models import UserProfile
 
 
 # Create your models here.
 class Conversation(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.ManyToManyField(User, related_name="conversations")
+    user = models.ManyToManyField(UserProfile, related_name="conversations")
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
@@ -18,10 +18,13 @@ class ConversationMessage(models.Model):
         Conversation, related_name="messages", on_delete=models.CASCADE
     )
     sender = models.ForeignKey(
-        User, related_name="sent_messages", on_delete=models.SET_NULL, null=True
+        UserProfile, related_name="sent_messages", on_delete=models.SET_NULL, null=True
     )
     receiver = models.ForeignKey(
-        User, related_name="received_messages", on_delete=models.SET_NULL, null=True
+        UserProfile,
+        related_name="received_messages",
+        on_delete=models.SET_NULL,
+        null=True,
     )
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- backend/chat/serializers.py
+++ backend/chat/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 from .models import Conversation, ConversationMessage
-from userprofile.serializers import UserProfileSerializer
+from userprofile.serializers import SimpleUserProfileSerializer
 
 
 class ConversationSerializer(serializers.ModelSerializer):
-    other_user = UserProfileSerializer(read_only=True)
+    other_user = SimpleUserProfileSerializer(read_only=True)
 
     class Meta:
         model = Conversation
-        fields = ("id", "other_user", "created_at", "modified_at")
+        fields = ("id", "user", "created_at", "modified_at")
 
 
 class ConversationMessageSerializer(serializers.ModelSerializer):
