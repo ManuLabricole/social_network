c377a292d01292adc48366aa372f3600694b7ae8
Manu Labricole <manu_barriol@hotmail.fr>
2023-10-19 19:35:58 +0200

    manage to print the 5 top trend - vali

--- frontend/src/components/feed/Trends.vue
+++ frontend/src/components/feed/Trends.vue
@@ -1,64 +1,18 @@
 <template>
-	<div class="p-4 bg-white border border-gray-200 rounded-lg">
+	<div
+		class="p-4 bg-white border border-gray-200 rounded-lg"
+		v-if="isFetched">
 		<h3 class="mb-6 text-xl">Trends</h3>
-		<div class="space-y-4">
-			<div class="flex items-center justify-between">
+		<div
+			class="space-y-4"
+			v-for="trend in trends"
+			:key="trend.id">
+			<div class="flex items-center justify-between mb-6">
 				<div class="flex items-center space-x-2">
 					<p class="text-xs">
-						<strong>#codewithstein</strong><br />
-						<span class="text-gray-500">180 posts</span>
-					</p>
-				</div>
-				<a
-					href="#"
-					class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg"
-					>Explore</a
-				>
-			</div>
-			<div class="flex items-center justify-between">
-				<div class="flex items-center space-x-2">
-					<p class="text-xs">
-						<strong>#codewithstein</strong><br />
-						<span class="text-gray-500">180 posts</span>
-					</p>
-				</div>
-				<a
-					href="#"
-					class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg"
-					>Explore</a
-				>
-			</div>
-			<div class="flex items-center justify-between">
-				<div class="flex items-center space-x-2">
-					<p class="text-xs">
-						<strong>#codewithstein</strong><br />
-						<span class="text-gray-500">180 posts</span>
-					</p>
-				</div>
-				<a
-					href="#"
-					class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg"
-					>Explore</a
-				>
-			</div>
-			<div class="flex items-center justify-between">
-				<div class="flex items-center space-x-2">
-					<p class="text-xs">
-						<strong>#codewithstein</strong><br />
-						<span class="text-gray-500">180 posts</span>
-					</p>
-				</div>
-				<a
-					href="#"
-					class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg"
-					>Explore</a
-				>
-			</div>
-			<div class="flex items-center justify-between">
-				<div class="flex items-center space-x-2">
-					<p class="text-xs">
-						<strong>#codewithstein</strong><br />
-						<span class="text-gray-500">180 posts</span>
+						<strong>#{{ trend.hashtag }}</strong
+						><br />
+						<span class="text-gray-500">{{ trend.count }} posts</span>
 					</p>
 				</div>
 				<a
@@ -74,20 +28,24 @@
 <script>
 	import axios from 'axios';
 	export default {
-		setup() {
-			return {};
+		data() {
+			return {
+				trends: [],
+				isFetched: false,
+			};
 		},
 
 		mounted() {
 			this.fetchTrends();
 		},
-
 		methods: {
 			fetchTrends() {
 				axios
 					.get(`/api/v1/posts/trends`)
 					.then((response) => {
-						console.log(response.data);
+						this.trends = response.data;
+						this.isFetched = true;
+						console.log('isFetched', this.isFetched);
 					})
 					.catch((error) => {
 						console.log(error);
