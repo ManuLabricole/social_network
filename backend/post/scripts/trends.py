5a584dfaaeb397a6232cebd1f333a25b97e79490
Manu Labricole <manu_barriol@hotmail.fr>
2023-10-20 09:15:58 +0200

    manage to print the 5 top trend - vali

--- backend/post/scripts/trends.py
+++ backend/post/scripts/trends.py
@@ -18,7 +18,11 @@ def compute_top_hashtags():
     posts = Post.objects.all().values_list("body", flat=True)
 
     # Extract hashtags from each post
-    hashtags = [hashtag for post in posts for hashtag in re.findall(r"#(\w+)", post)]
+    hashtags = [
+        hashtag.lower().replace(".", "")
+        for post in posts
+        for hashtag in re.findall(r"#(\w+)", post)
+    ]
 
     # Compute the ten most frequent hashtags
     top_hashtags = Counter(hashtags).most_common(5)
